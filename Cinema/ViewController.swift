f4430a7a6db3eae51bc71df85663d908f89872e6
Bruno Meneghin <brunomeneghin@icloud.com>
2021-08-10 11:56:45 -0300

    Initial Commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Bruno Meneghin on 10/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
